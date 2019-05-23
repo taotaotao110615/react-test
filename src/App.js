6710949030c7d4db6cebcf9887a75db77d0de921
Yinai <935827611@qq.com>
2019-05-23 09:42:22 +0800

    首次上传

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
 import './App.css';
-import Ahead from './head/Ahead';
-import Xiaoxi from "./cont/xiaoxi/xiaoxi";
-import Footer from "./foot/footer";
-import Carousel from "./cont/Carousel/carousel";
-import Purchase from "./cont/purchase/purchase";
-
 function App() {
   return (
     <div className="App">
-        <Ahead></Ahead>
-        <div className="cont-box">
-          <div className="cont-one">
-                <Xiaoxi></Xiaoxi>
-              <div className="lunbo">
-                  <Carousel></Carousel>
-                  <Purchase></Purchase>
-              </div>
-          </div>
-        </div>
-      <Footer></Footer>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './reset.css'
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
